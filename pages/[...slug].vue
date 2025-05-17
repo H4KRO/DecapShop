92da405f926f43748e4f6f0974f274d3a36a3612
Eddie Vallier <32486404+H4KRO@users.noreply.github.com>
2025-05-17 17:07:07 +0200

    first commit

---
+++ pages/[...slug].vue
@@ -0,0 +1,17 @@
+<script setup>
+const route = useRoute()
+
+console.log(route.path)
+const path = `/${['pages', ...route.path.split('/')].filter(r => r != '').join('/')}`
+
+const { data } = await useAsyncData('page', () => queryCollection('pages').path(path).first())
+</script>
+
+<template>
+  <div v-if="data">
+    <ContentRenderer :value="data" />
+  </div>
+  <div v-else>
+    <h1>Page non trouvée</h1>
+  </div>
+</template>
